3e97d3a25d1b2516a4c903f260e9604c71fc837d
irenegarciaman <157684882+irenegarciaman@users.noreply.github.com>
2024-02-21 08:54:03 +0100

    Todo hasta el 2.8

---
+++ Test/Java/AuthorTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorTest {
+
+
+        @Test
+        void getEmail () {
+            Author a1 = new Author("Tan Ah Teck", "ahteck@nowhere.com");
+            System.out.println("email is:" + a1.getEmail());
+        }
+
+        @Test
+        void setEmail () {
+            Author a2 = new Author("Tan Ah Teck", "ahteck@nowhere.com");
+            a2.setEmail("ahteck@somewhere.com");
+        }
+
+        @Test
+        void getName () {
+            Author a1 = new Author("Tan Ah Teck", "ahteck@nowhere.com");
+            System.out.println("name is:" + a1.getName());
+        }
+
+        @Test
+        void testToString () {
+            Author a2 = new Author("Tan Ah Teck", "ahteck@nowhere.com");
+            a2.setEmail("ahteck@somewhere.com");
+        }
+
+}
---
+++ src/MyPoint.java
@@ -0,0 +1,54 @@
+public class MyPoint {
+    private int x=0;
+    private int y=0;
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public MyPoint(){}
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int[] getXY(){
+        int numeros[] = {getX(),getY()};
+        return numeros;
+    }
+    public void setXY(int x,int y){
+        setX(x);
+        setY(y);
+    }
+
+    @Override
+    public String toString() {
+        return "MyPoint[" + "x=" + x + ", y=" + y + ']';
+    }
+    public double distance(int xi,int yi){
+        int distX= xi- x;
+        int distY= yi - y;
+        return Math.sqrt((distX^2)+(distY^2));
+    }
+    public double distance(MyPoint another) {
+        int xi = another.getX();
+        int yi = another.getY();
+        int distX = xi - x;
+        int distY = yi - y;
+        return Math.sqrt((distX ^ 2) + (distY ^ 2));
+    }
+    public double distance(){
+        return Math.sqrt((x^2)+(y^2));
+    }
+}
