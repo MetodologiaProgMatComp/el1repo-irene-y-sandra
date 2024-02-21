2b8fb72600c2e44af2c23a5d4830166e34d3945a
sandradomdi <157640701+sandradomdi@users.noreply.github.com>
2024-02-21 21:35:14 +0100

    Cambios hoja 2

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclase/hoja2/Customer4.java
@@ -0,0 +1,34 @@
+package es.uah.matcomp.mp.e1.ejerciciosclase.hoja2;
+
+public class Customer4 {
+    private int id;
+    private String name;
+    private int discount;
+
+    @Override
+    public String toString() {
+        return "[" + name + "(" + id + ")(" + discount + "%)]";
+    }
+
+    public Customer4(int id, String name, int discount) {
+        this.id = id;
+        this.name = name;
+        this.discount = discount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+}
