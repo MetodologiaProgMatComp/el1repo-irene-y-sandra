056f1a23c52371f03ad0d37b948c527fb95f0dc6
irenegarciaman <157684882+irenegarciaman@users.noreply.github.com>
2024-04-10 21:15:43 +0200

    gson

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclase/gson/Gson1.java
@@ -0,0 +1,29 @@
+package es.uah.matcomp.mp.e1.ejerciciosclase.gson;
+
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Gson1 {
+    // Método para guardar un objeto en un archivo JSON
+    public static <T> void guardarObjetoEnArchivo(String rutaArchivo, T objeto) {
+        Gson gson = new Gson();
+        try (FileWriter writer = new FileWriter(rutaArchivo)) {
+            gson.toJson(objeto, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    // Método para cargar un objeto desde un archivo JSON
+    public static <T> T cargarObjetoDesdeArchivo(String rutaArchivo, Class<T> clase) {
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader(rutaArchivo)) {
+            return gson.fromJson(reader, clase);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
